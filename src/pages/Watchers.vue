43a36ad20ed24ce8e8c43f1ae2be9184165a92df
ybkja26 <ybkja26@gmail.com>
2024-02-14 10:11:40 +0800

    new

--- src/pages/Watchers.vue
+++ src/pages/Watchers.vue
@@ -1,43 +1,32 @@
 <template>
   <div>
-    <h1>this is watchers</h1>
-    <h1>Volume Tracker - (0-20)</h1>
-    <small v-if="volume === alertVolume">Danger</small>
-    <h2>the volume is {{ volume }}</h2>
-    <div>
-      <button @click="volume += 1">INCREMENT</button>
-      <button @click="volume -= 1">DECREMENT</button>
+    <div class="main-watchers">
+      <div>
+        <h1>this is watchers</h1>
+        <h1>Volume Tracker - (0-20)</h1>
+        <small v-if="volume === alertVolume">Danger</small>
+        <h2>the volume is {{ volume }}</h2>
+        <div>
+          <button @click="volume += 1">INCREMENT</button>
+          <button @click="volume -= 1">DECREMENT</button>
+        </div>
+      </div>
+      <div>
+        <h1>{{ name }}</h1>
+        <button @click="changeName">change</button>
+      </div>
     </div>
   </div>
 </template>
 
-<!-- <script>
-export default {
-  data() {
-    return {
-      volume: 0,
-      alertVolume: 16,
-    };
-  },
-  methods: {},
-  computed: {},
-  watch: {
-    volume(a, b) {
-      if (a > b && a === 16) {
-        alert("your volume is high might damage your hearing");
-      }
-    },
-  },
-};
-</script> -->
-
 <script>
-import { ref, watch } from 'vue';
+import { ref, watch } from "vue";
 
 export default {
   setup() {
     const volume = ref(0);
     const alertVolume = ref(16);
+    const name = ref('ralph');
 
     watch(volume, (newVolume, oldVolume) => {
       if (newVolume > oldVolume && newVolume === alertVolume.value) {
@@ -45,15 +34,30 @@ export default {
       }
     });
 
+
+    watch(name,(newValue, oldValue) => {
+      console.log(`this is the ${oldValue} and this is the ${newValue}`)
+    })
+
+    const changeName = () => {
+      name.value = 'santolorin'
+    }
+
     return {
       volume,
       alertVolume,
+      name,
+      changeName
     };
   },
 };
 </script>
 
-
-<style></style>
-
-
+<style>
+.main-watchers {
+  display: flex;
+  border: 1px solid red;
+  width: 70vw;
+  justify-content: space-between;
+}
+</style>
