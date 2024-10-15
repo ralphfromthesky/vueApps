013e6bbddf5bbfba1fa2c9a6bbf64e053f070e89
ralphfromthesky <ralphsantolorin53@gmail.com>
2024-10-15 10:54:29 +0800

    new

--- dist/assets/transitionEffect-e1d29467.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c,a as o}from"./index-419621f3.js";const n={},s=o("div",null,"keepalive",-1),a=[s];function r(_,i){return t(),c("div",null,a)}const d=e(n,[["render",r]]);export{d as default};
--- src/views/quizzApp2.vue
+++ src/views/quizzApp2.vue
@@ -1,27 +1,35 @@
 <template>
   <div>
-    <div>QUIZ APP {{ rightAns }}</div>
-    <div v-for="(q, index) in quizQuestions" :index="q.index" v-if="hideQuestion">
-      <div v-if="index === selectedQuestion">
-        <div>Question - {{ q.index }}/10 - {{ selectedQuestion }}</div>
-        <div>
-          {{ q.question }}
-        </div>
+    <div class="text-center text-[2rem] font-bold">SIMPLE QUIZ APP</div>
+    <div v-if="hideQuestion">
+      <div v-for="(q, index) in quizQuestions" :index="q.index" bor>
+          <div v-if="index === selectedQuestion" class="text-[1.8rem]">
+            <div class="flex justify-end text-[1rem]">
+              Question - {{ q.index }}/10
+            </div>
+            <div class="my-2">
+              {{ q.question }}
+            </div>
 
-        <div
-          v-for="(o, i) in q.options"
-          :class="[
-            o === selected ? 'bg-gray-200 border-red-200' : '',
-            'leading-[2.5rem]',
-          ]"
-          :key="index"
-          @click="selectAns(o, q.answer)"
-        >
-          <div>{{ o }}</div>
-        </div>
-        <!-- <div class="border-2 border-[red]">{{ q.answer }}</div> -->
+            <div
+              v-for="(o, i) in q.options"
+              :class="[
+                o === selected
+                  ? 'bg-gray-200 text-[#000000] border-red-200'
+                  : ' text-[#7a7a7a]',
+                'leading-[2.5rem] my-2 text-[1.5rem]',
+              ]"
+              :key="index"
+              @click="selectAns(o, q.answer)"
+            >
+              <div>{{ o }}</div>
+            </div>
+            <div class="border-2 border-[red]">{{ q.answer }}</div>
+          </div>
+      </div>
+      <div class="text-center">
         <button
-          class="p-1 rounded border-2"
+          class="p-1 rounded-[.5rem] w-full border-2 border-[gray] my-5"
           :disabled="lastQuestion"
           @click="switchQuestion()"
           v-if="selectedQuestion < 9"
@@ -29,8 +37,7 @@
           Next question...
         </button>
         <button
-          class="p-1 rounded border-2"
-          :disabled="lastQuestion"
+          class="p-1 rounded bg-[green] w-full text-white border-2"
           @click="submitAns()"
           v-if="selectedQuestion === 9"
         >
@@ -38,8 +45,15 @@
         </button>
       </div>
     </div>
-    <div v-else>
-
+    <div v-else class="text-[1.6rem]">
+      CONGRATULATION!!! you got {{ rightAns }} out of 10
+      {{ rightAns <= 5 ? "you failed the tests" : "you pass the test!" }}
+      <div class="flex justify-center mt-10" v-if="rightAns <= 5">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSerNfJr-vJiOL5j90bVNFehH8_y8U2U_DR5g&s" alt="">
+      </div>
+      <div class="flex justify-center mt-10" v-if="rightAns >= 6">
+        <img src="https://i.pinimg.com/originals/86/d1/76/86d1767ba3ecb6af8df3e4e5dda376eb.gif" alt="">
+      </div>
     </div>
   </div>
 </template>
@@ -70,12 +84,12 @@ const switchQuestion = () => {
     return;
   }
   selectedQuestion.value++;
-  //selected.value = null
+   selected.value = null;
 };
 
 const submitAns = () => {
   hideQuestion.value = false;
-  alert('fdasdf')
+  alert("you finished the test!");
 };
 
 const lastQuestion = computed(() => {
@@ -151,4 +165,22 @@ const quizQuestions = ref([
 ]);
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.route-enter-from {
+  opacity: 0;
+  transform: translateX(10rem);
+}
+
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.route-leave-to {
+  opacity: 0;
+  transform: translateX(-10rem);
+}
+
+.route-leave-active {
+  transition: all 0.3s ease-in;
+}
+</style>
