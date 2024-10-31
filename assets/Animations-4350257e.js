7d2239b9a0c92fc461282cec3937975c950b05f0
ralphfromthesky <ralphsantolorin53@gmail.com>
2024-10-31 10:59:57 +0800

    Updates

--- assets/Animations-4350257e.js
+++
@@ -1,1 +0,0 @@
-import{S as o}from"./SlidingIn-b73d51dc.js";import{_ as e,s as t,o as s,c as a,y as c}from"./index-900186c7.js";const i={name:"animation",components:{SlidingIn:o}};function r(m,p,_,l,d,f){const n=t("SlidingIn");return s(),a("div",null,[c(n)])}const $=e(i,[["render",r]]);export{$ as default};
